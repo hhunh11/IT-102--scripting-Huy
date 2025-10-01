6b321455a4cc32e76f814bbce036de1329473461
huy huynh <huynh.huy@student.greenriver.edu>
2025-09-30 23:08:23 -0700

    updating HelloWorld.py file

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
 # Created 
+name=input("Enter your name: ")
+print("Hello "+ name)
+print("Today is going to be a great day!")
+print("--------------------")
+age=int(input("Enter you age: "))
+print("In 2 years, you will be",age+2,"years old")
