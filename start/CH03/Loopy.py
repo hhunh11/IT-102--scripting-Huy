f405b6703976b656a79eb7d79576079aa22f2a14
Huy <huynh.huy@student.greenriver.edu>
2025-10-16 09:48:08 -0700

    conditionals and loopy updating

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 # example workign with Loops
 #By 
+answer=input("Is today a good day? (y/n) ").lower()
+if answer=="y":
+    for i in range(10):
+        print("Yeah it is")
