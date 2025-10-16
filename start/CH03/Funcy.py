00c44efd11eac9ca945bea73dd5aea9a31d16970
Huy <huynh.huy@student.greenriver.edu>
2025-10-16 10:50:20 -0700

    Funcy updating

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # example workign with Functions
 #By 
+def send_message(times):
+    for i in range(times):
+        print("Yeah it is")
+answer=input("Is today a good day? (y/n) ").lower()
+if answer=="y":
+    send_message(10)
