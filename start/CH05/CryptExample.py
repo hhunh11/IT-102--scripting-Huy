6aee65ea9f85a5798d6bcaccdfaced0d73884496
huy huynh <huynh.huy@student.greenriver.edu>
2025-10-27 00:35:37 -0700

    Crypt and Rot13 updated

--- start/CH05/CryptExample.py
+++ start/CH05/CryptExample.py
@@ -1,3 +1,34 @@
 #!/usr/bin/env python3
 # Script that encrypts/decrypts text using cryptography module
 # By 
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.hashes import SHA256
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from cryptography.hazmat.backends import default_backend
+from base64 import b64encode, b64decode
+import os
+
+key=os.urandom(16)
+print("Generated AES Key:", key)
+
+plaintext=input("Enter a plaintext message: ").encode()
+padder=padding.PKCS7(128).padder()
+padded_data=padder.update(plaintext)+padder.finalize()
+
+iv=os.urandom(16)
+
+cipher=Cipher(algorithms.AES(key),modes.CBC(iv),backend=default_backend())
+encryptor=cipher.encryptor()
+ciphertext=encryptor.update(padded_data)+encryptor.finalize()
+
+print("Encrypted message: ",b64encode(ciphertext).decode())
+
+decryptor=cipher.decryptor()
+decryptext=decryptor.update(ciphertext)+decryptor.finalize()
+
+unpadder=padding.PKCS7(128).unpadder()
+unpadded_data=unpadder.update(decryptext)+unpadder.finalize()
+
+print("Decrypted message: "+unpadded_data.decode())
--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 # Script that encrypts/decrypts text using ROT13
 # By 
+def rot13(text,shift):
+    result=""
+    for char in text:
+        if char.isalpha():
+            base= ord("a") if char.islower() else ord("A")
+            result+=chr((ord(char)-base+shift) %26+base)
+        else: result+=char
+    return result
+
+message=input("Enter a message: ")
+shift=int(input("Enter the shift value: "))
+
+encrypted=rot13(message,shift)
+decrypted=rot13(encrypted,-shift)
+
+print("Encrypted message: "+encrypted)
+
+answer=input("Decrypt the message? (yes/no): ").lower()
+if answer=="yes":
+    print("Decrypted message: "+decrypted)
